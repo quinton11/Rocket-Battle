4bf6a5028691aabded0aa24056e6df0ae06b61c2
quinton11 <quintons831@gmail.com>
2022-11-19 22:05:16 +0000

    Home screen menus

--- src/Screens/HomeScreen.cpp
+++ src/Screens/HomeScreen.cpp
@@ -14,8 +14,36 @@ void HomeScreen::render(SDL_Renderer *renderer)
 		SDL_RenderCopy(renderer, screentexture, NULL, NULL);
 		SDL_RenderCopy(renderer, play, NULL, &play_rect);
 		SDL_RenderPresent(renderer);
-	}
 
+		/*
+		TODO:
+			- Render main menu with play, settings and quit buttons
+			- sub menu in play menu which renders when play is clicked
+			- under play menu, have a new player button, select player button if players exist
+			- and back button.
+			- under new player button have a text entry field for entering new player's name
+			- when new player is added move to select a player menu
+			- under select a player's menu we display the existing players and their high score
+			- user can toggle through and select a player
+			- after selecting, user presses enter and menu changes to start
+			- start menu has start game button and back
+			- when start game is clicked, user moves into game
+		 */
+	}
+}
+
+void HomeScreen::renderMenu(SDL_Renderer *renderer)
+{
+	// using screenwidth and screenheight calculate the x,y,w and h of the container
+	// that will be a rect, render the rect.
+	// Then we check which menu is active
+	// if active menu pointer is not pointing to the active menu we point to it
+	// then we point the activeButtons to its buttons
+	// since its active we create their rects according to the structure
+	// since its fixed we need not create it multiple times, we can create it and store in memory
+	// Each button has same width height and x coordinates, y however differs depending on which button
+	// follows which.
+	// we create their rects in order, then their colours and render them
 }
 
 void HomeScreen::eventchecker()
@@ -32,11 +60,21 @@ void HomeScreen::eventchecker()
 	case SDL_MOUSEMOTION:
 		mouse_x = events.motion.x;
 		mouse_y = events.motion.y;
+		// if there is a mouse motion we check in the active menus buttons for which
+		// button the mouse's coordinates is in,then we set the isactive flag which triggers
+		// a color change
+		// so using the mouse in play function we can check which button the mouse is in
 
 	case SDL_MOUSEBUTTONDOWN:
 		// std::cout << "Mouse clicker" << std::endl;
 		if (events.button.button == SDL_BUTTON_LEFT && HomeScreen::mouse_in_play(mouse_x, mouse_y, play_rect))
 		{
+			// if mouse left button is clicked.
+			// check for active menu
+			// if active menu is main menu then we check which button in its buttons was the mouse in
+			// then we trigger the buttons selected flag and change the menus active state to the
+			// subsequent one.
+			// Ex if start was clicked, we toggle the main menu to false and start to true
 			std::cout << "Start game clicked" << std::endl;
 			ismounted = false;
 		}
--- src/Screens/HomeScreen.h
+++ src/Screens/HomeScreen.h
@@ -1,15 +1,51 @@
 #pragma once
 #include "Screen.h"
 #include "..\GameManager\TextureManager.h"
+#include <string>
+#include "list"
+
+struct Button
+{
+	std::string name;
+	bool isActive;
+	bool isSelected;
+};
+
+struct Menu
+{
+	std::string name;
+	std::list<Button> buttons;
+	bool active;
+};
 
 class HomeScreen : public Screen
 {
 
 public:
 	TextureManager textm;
+	// Main menu
+	Button start = {"start", false, false};
+	Button settings = {"settings", false, false};
+	Button mquit = {"quit", false, false};
+	Menu main = {"Main-Menu", {start, settings, mquit}, true};
+
+	// Start menu
+	Button newPlayer = {"New Player", false, false};
+	Button selectPlayer = {"Select Player", false, false};
+	Button highScore = {"High Score", false, false};
+	Button back = {"Back", false, false};
+	Menu Start = {"Start-Menu", {newPlayer, selectPlayer, highScore, back}, false};
+
+	// Play Menu
+	Button play = {"Play", false, false};
+	Button msettings = {"Settings", false, false};
+	Button pback = {"Back", false, false};
+	Menu Play = {"Play-Menu", {play, msettings, pback}, false};
 
 private:
 	bool ismounted = true;
+	std::list<Button> *activeButtons;
+	Menu *activeMenu;
 	bool quit = false;
 	SDL_Texture *screentexture = nullptr;
 	SDL_Event events;
@@ -18,6 +54,7 @@ private:
 
 public:
 	void render(SDL_Renderer *renderer);
+	void renderMenu(SDL_Renderer *renderer);
 	void eventchecker();
 
 	bool getismounted();
